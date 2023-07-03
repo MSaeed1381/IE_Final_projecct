7abd272c57c69fe44a083b24e1d39255060ac488
Saeed Zare <44446617+MSaeed1381@users.noreply.github.com>
2023-07-03 09:14:28 +0330

    login message adds

--- golestan/src/pages/Login.js
+++ golestan/src/pages/Login.js
@@ -20,34 +20,65 @@ export default function Login() {
         const user_id = userRef.current.value;
         const pass = passwordRef.current.value;
 
-        let data = await axios({
-            // Endpoint to send files
-            url: "http://127.0.0.1:3000/login",
-            method: "POST",
-            headers: {
-                // Add any auth token here
-                'Access-Control-Allow-Origin': 'http://127.0.0.1:3001',
-                'Access-Control-Allow-Methods': 'POST',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Content-Type': 'application/json',
 
-                //authorization: "your token comes here",
-            },
+        const $ = document;
+        let login_message = $.querySelector('.fail-success');
+        try {
+            let data = await axios({
+                // Endpoint to send files
+                url: "http://127.0.0.1:3000/login",
+                method: "POST",
+                headers: {
+                    // Add any auth token here
+                    'Access-Control-Allow-Origin': 'http://127.0.0.1:3001',
+                    'Access-Control-Allow-Methods': 'POST',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Content-Type': 'application/json',
 
-            // Attaching the form data
-            data: {
-                'user_id': user_id,
-                'password': pass
-            },
-        });
+                    //authorization: "your token comes here",
+                },
 
-        data = data.data
-        // Compare user info
-        if (data.success) {
-            console.log(data);
-            navigate('/admin');
-        } else {
+                // Attaching the form data
+                data: {
+                    'user_id': user_id,
+                    'password': pass
+                },
+            });
+
+            data = data.data
+            // Compare user info
+
+            if (data.success) {
+                console.log(data);
+                if (data.body.userRole === 'itManagers'){
+                    console.log("admin");
+                    login_message.style.visibility = 'visible';
+                    login_message.innerHTML = 'شما به عنوان ادمین با موفقیت وارد خواهد شد';
+                    login_message.style.backgroundColor = 'green';
+
+                    setTimeout(() => {
+                        navigate('/admin');
+                    }, 3500);
+
+                } else if (data.body.userRole === 'student')
+                    navigate('/student')
+            } else {
+                console.log('username of password is not correct');
+                login_message.innerHTML = 'شماره کاربری یا رمز ورود اشتباه است.';
+                login_message.style.backgroundColor = 'red';
+                login_message.style.visibility = 'visible';
+                setTimeout(() => {
+                    login_message.style.visibility = 'hidden';
+                }, 3500);
+            }
+        }catch (e) {
             console.log('username of password is not correct');
+            login_message.innerHTML = 'شماره کاربری یا رمز ورود اشتباه است.';
+            login_message.style.backgroundColor = 'red';
+            login_message.style.visibility = 'visible';
+            setTimeout(() => {
+                login_message.style.visibility = 'hidden';
+            }, 1000);
         }
     };
 
@@ -63,15 +94,17 @@ export default function Login() {
                     </div>
 
                     <label>
-                        <input name="user_id" placeholder="شناسه کاربری" ref={userRef}/>
+                        <input name="user_id" placeholder="شناسه کاربری" ref={userRef} required/>
                     </label>
 
                     <label>
-                        <input name="password" placeholder="گذرواژه" type="password" ref={passwordRef}/>
+                        <input name="password" placeholder="گذرواژه" type="password" ref={passwordRef} required/>
                     </label>
 
 
                     <input type="submit" value="ورود"/>
+
+                    <p className="fail-success"> </p>
                 </fieldset>
             </form>
         </div>
